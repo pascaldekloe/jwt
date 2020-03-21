4f01f59ea9a18b73c6ecfdec5aa04646940490ed
Pascal S. de Kloe <pascal@quies.net>
2020-03-21 12:45:07 +0100

    Test millisecond resolution for Valid.

--- jwt_test.go
+++ jwt_test.go
@@ -180,32 +180,32 @@ func TestNumericTimeMapping(t *testing.T) {
 func TestClaimsValid(t *testing.T) {
 	c := new(Claims)
 	if !c.Valid(time.Time{}) {
-		t.Error("invalidated claims without time limits for zero")
+		t.Error("invalidated claims without time limits for zero time")
 	}
 	if !c.Valid(time.Now()) {
 		t.Error("invalidated claims without time limits")
 	}
 
 	now := time.Now()
-	c.Registered.NotBefore = NewNumericTime(now)
-	c.Registered.Expires = NewNumericTime(now.Add(time.Minute))
+	c.NotBefore = NewNumericTime(now)
+	c.Expires = NewNumericTime(now.Add(time.Minute))
 
 	if c.Valid(time.Time{}) {
 		t.Error("validated claims with time limits for zero time")
 	}
-	if c.Valid(c.Registered.NotBefore.Time().Add(-time.Second)) {
+	if c.Valid(c.NotBefore.Time().Add(-time.Millisecond)) {
 		t.Error("validated claims before time limit")
 	}
-	if !c.Valid(c.Registered.NotBefore.Time()) {
+	if !c.Valid(c.NotBefore.Time()) {
 		t.Error("invalidated claims on time limit start")
 	}
-	if !c.Valid(c.Registered.NotBefore.Time().Add(time.Second)) {
+	if !c.Valid(c.NotBefore.Time().Add(time.Millisecond)) {
 		t.Error("invalidated claims within time limit")
 	}
-	if c.Valid(c.Registered.Expires.Time()) {
+	if c.Valid(c.Expires.Time()) {
 		t.Error("validated claims on time limit end")
 	}
-	if c.Valid(c.Registered.Expires.Time().Add(time.Second)) {
+	if c.Valid(c.Expires.Time().Add(time.Millisecond)) {
 		t.Error("validated claims after time limit end")
 	}
 }
